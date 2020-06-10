7d43d838d22fa3e1f73a4f96a1290f2e619869e5
23rdmaxim <64205435+23rdmaxim@users.noreply.github.com>
2020-06-10 12:31:11 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,7 +38,7 @@ async def help_message_f(client, message):
     message_id = 99
     # display the /help message
     await message.reply_text(
-        f"please read the <a href='https://t.me/c/{channel_id}/{message_id}'>Pinned Message</a>",
+        f"Hey there!/nUse the following commands/n/leech (as a reply to a magnet/torrent/direct link/n/ytdl (youtube link)/n Thank you for using me😊",
         quote=True
     )
 
